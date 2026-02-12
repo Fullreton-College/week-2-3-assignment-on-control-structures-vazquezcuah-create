0f8b6f5291a2a19de7f706e6fc3d599dfb72b91b
Cuahutemoc Vazquez <vazquez.cuah@gmail.com>
2026-02-12 01:23:24 -0800

    Update SoftwareStore.cpp

--- SoftwareStore.cpp
+++ SoftwareStore.cpp
@@ -3,14 +3,36 @@
 using namespace std;
 int main()
 {
-    double mph = 6.5;
-    // PACE calculation
-    /* write your code */
-
-    //express pace in minutes and seconds
-  /* write your code here */
-
-  //express your output in 1 decimal points
-
-
+double price =199;
+double discount=1.0;
+int units;
+cout<<"Enter the amount of units you would like in order to recieve a quote: "<<endl;
+cin>>units;
+if(units<10){
+  price*=units;
+}else if((units>=10)&&(units<=19)){
+  discount-=.2;
+  price*=units;
+  price*=discount;
+}else if((units>=20)&&(units<=40)){
+  discount-=.3;
+  price*=units;
+  price*=discount;
+}else if((units>=50)&&(units<=99)){
+  discount -=.4;
+  price*=units;
+  price*=discount;
+}else if(units>=100) {
+  discount -= .5;
+price*=units;
+price*=discount;
+}
+discount-=1;
+discount*=-100;
+cout<<"With a puchase of "<<units<<" units";
+if(discount>0){
+  cout<<", you recieved a discount of "<<discount<<"% and so";
+}
+cout<< " your total estimated amount is $"<<price<<"."<<endl;
+  return 0;
 }
